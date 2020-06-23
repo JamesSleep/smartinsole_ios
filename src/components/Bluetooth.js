7cd767de674e532b27094a242befb609fc749c6b
jamessleep <tyuyiy@naver.com>
2020-06-23 13:57:09 +0900

    Bluetooth Fixed Responsive

--- src/components/Bluetooth.js
+++ src/components/Bluetooth.js
@@ -1,17 +1,18 @@
 //import BluetoothSerial from 'react-native-bluetooth-serial-next';
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, ToastAndroid,NativeModules, NativeEventEmitter } from 'react-native'
+import { Text, View, Dimensions } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { BleManager } from "react-native-ble-plx"
 
+const _WIDTH = Dimensions.get('window').width;
+const _HEIGHT = Dimensions.get('window').height;
 
 const LEFT_INSOLE_NAME = "left insole"; 
 const RIGHT_INSOLE_NAME = "right insole";
 
 function Bluetooth({navigation}) { 
-
     const [devices, setDevices] = useState([]);
     const [LeftDevice, setLeft] = useState({id:"",name:"",isConnect:false});
     const [RightDevice, setRight] = useState({id:"",name:"",isConnect:false});
@@ -20,8 +21,6 @@ function Bluetooth({navigation}) {
     const [uuid ,setUuid] =useState("");
     const [serviceUUIDs ,setsServiceUUIDs] =useState([]);
     const [state ,setState] =useState("PoweredOn");
-
-
     useEffect(() => {
         /* console.log("gogo")
         let states = manager.onStateChange();
@@ -66,30 +65,30 @@ function Bluetooth({navigation}) {
     return (
         <LinearGradient start={{x: 1.5, y: 0}} end={{x: 0, y: 0}} colors={['#B2FEFA', '#0ED2F7']} style={{flex:1, alignItems:"center"}} >
 			<MainView>
-				<Text style={{fontSize:22, fontWeight:"bold"}}>블루투스 연결</Text>
+				<Text style={{fontSize:_WIDTH/23, fontWeight:"bold"}}>블루투스 연결</Text>
                 <View style={{marginTop:30}}>
-                    <Text style={{fontSize:19, fontWeight:"bold"}}>왼쪽, 오른쪽 인솔 장치 연결</Text>
-                    <Text style={{fontSize:15, color:"#d1ccc0"}}>스마트폰의 블루투스 악세서리를 연결해주세요</Text>
+                    <Text style={{fontSize:_WIDTH/28, fontWeight:"bold"}}>왼쪽, 오른쪽 인솔 장치 연결</Text>
+                    <Text style={{fontSize:_WIDTH/28, color:"#d1ccc0"}}>스마트폰의 블루투스 악세서리를 연결해주세요</Text>
                 </View>
                 <View style={{flexDirection:"row",height:"100%",justifyContent:"space-around"}}>
                     <BluetoothBox>
-                        <Text style={{fontSize:16, fontWeight:"bold"}}>왼쪽 인솔 연결</Text>
-                        <Text style={{fontSize:15, color:"#d1ccc0"}}>{LeftDevice.name===""?"LEFT":LeftDevice.name}</Text>
+                        <Text style={{fontSize:_WIDTH/30, fontWeight:"bold"}}>왼쪽 인솔 연결</Text>
+                        <Text style={{fontSize:_WIDTH/33, color:"#d1ccc0"}}>{LeftDevice.name===""?"LEFT":LeftDevice.name}</Text>
                         <View style={{justifyContent:"center",alignItems:"center",height:"50%"}}>
-                            <Icon name="check-circle" size={40}  color={LeftDevice.isConnect?"#34ace0":"#d1ccc0"} />
+                            <Icon name="check-circle" size={_WIDTH/11}  color={LeftDevice.isConnect?"#34ace0":"#d1ccc0"} />
                         </View>
                     </BluetoothBox>
                     <BluetoothBox>
-                        <Text style={{fontSize:16, fontWeight:"bold"}}>오른쪽 인솔 연결</Text>
-                        <Text style={{fontSize:15, color:"#d1ccc0"}}>{RightDevice.name===""?"RIGHT":RightDevice.name}</Text>
+                        <Text style={{fontSize:_WIDTH/30, fontWeight:"bold"}}>오른쪽 인솔 연결</Text>
+                        <Text style={{fontSize:_WIDTH/33, color:"#d1ccc0"}}>{RightDevice.name===""?"RIGHT":RightDevice.name}</Text>
                         <View style={{justifyContent:"center",alignItems:"center",height:"50%"}}>
-                            <Icon name="check-circle" size={40}  color={RightDevice.isConnect?"#34ace0":"#d1ccc0"} />
+                            <Icon name="check-circle" size={_WIDTH/11}  color={RightDevice.isConnect?"#34ace0":"#d1ccc0"} />
                         </View>
                     </BluetoothBox>
                 </View>
 			</MainView>
             <NextBtn onPress={()=>navigation.navigate('MainRouter')}>
-                <Text style={{fontSize:22, fontWeight:"bold"}}>시작하기</Text>
+                <Text style={{fontSize:_WIDTH/22, fontWeight:"bold"}}>시작하기</Text>
             </NextBtn>
         </LinearGradient>
     ) 
@@ -98,23 +97,23 @@ function Bluetooth({navigation}) {
 const MainView = styled.View`
 	width : 90%;
     height : 50%;
-    padding : 30px 25px 20px 25px;
-	margin-top : 100px;
+    margin-top : ${_HEIGHT/10}px;
+    padding : 4% 5% 6% 5%;
 	background-color : white;
 `;
 const BluetoothBox = styled.View`
     width : 40%;
     height : 60%;
-    margin-top : 25px;
+    margin-top : ${_HEIGHT/30}px;
     padding : 10px;
     border : 3px #7f8c8d;
     border-radius : 20px;
 `;
 const NextBtn = styled.TouchableOpacity`
     width : 80%;
-    height : 55px;
+    height : ${_HEIGHT/13}px;
     position : absolute;
-    bottom : 50px;
+    bottom : ${_HEIGHT/15}px;
     background-color : white;
     border-radius : 30px;
     justify-content : center;
--- src/components/Home/FinderPW.js
+++ src/components/Home/FinderPW.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import {  View, Text , TouchableWithoutFeedback, Keyboard } from 'react-native'
+import {  View, Text , TouchableWithoutFeedback, Keyboard, Dimensions } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styled from 'styled-components';
-import Axios from 'axios';
+
+const _WIDTH = Dimensions.get('window').width;
+const _HEIGHT = Dimensions.get('window').height;
 
 function FinderPW({navigation}) {
     const [tempPW, setTemp] = useState(false);
@@ -11,11 +13,11 @@ function FinderPW({navigation}) {
         <LinearGradient start={{x: 1.5, y: 0}} end={{x: 0, y: 0}} colors={['#B2FEFA', '#0ED2F7']} style={{flex:1,justifyContent:"center",alignItems:"center"}}>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={{flex:1, width:"100%", height:"100%", justifyContent:"center",alignItems:"center"}}>
-                    <Icon name="angle-left" size={40} color="#fff" style={{position:"absolute",left:20,top:10}} onPress={()=>navigation.goBack()}/>
+                    <Icon name="angle-left" size={_WIDTH/13} color="#fff" style={{position:"absolute",left:20,top:10}} onPress={()=>navigation.goBack()}/>
                     <FinderBox>
                         <FinderTitle editable={false} value={"비밀번호 찾기"} />
-                        <Text style={{fontSize:18, fontWeight:"bold", paddingLeft:10}}>이메일주소를 입력해주세요</Text>
-                        <Text style={{fontSize:14, paddingLeft:10,marginBottom:20, color:"#95a5a6"}}>가입했던 이메일로 임시 비밀번호가 발급됩니다</Text>
+                        <Text style={{fontSize:_WIDTH/23, fontWeight:"bold", paddingLeft:10}}>이메일주소를 입력해주세요</Text>
+                        <Text style={{fontSize:_WIDTH/30, paddingLeft:10,marginBottom:_HEIGHT/35, color:"#95a5a6"}}>가입했던 이메일로 임시 비밀번호가 발급됩니다</Text>
                         <View style={{marginTop:10, flexDirection:"row", paddingLeft:10, alignItems:"center"}}>
                             <InputData placeholder="이메일주소를 입력해주세요" placeholderTextColor="gray"></InputData>
                         </View>
@@ -23,17 +25,17 @@ function FinderPW({navigation}) {
                     {   
                         tempPW?
                         <PostButton activeOpacity={1}>
-                            <Text style={{fontSize:22, color:"#95a5a6", textAlign:"center", opacity:0.7}}>임시 비밀번호 전송</Text>
+                            <Text style={{fontSize:_WIDTH/20, color:"#95a5a6", textAlign:"center", opacity:0.7}}>임시 비밀번호 전송</Text>
                         </PostButton>
                         :
                         <PostButton onPress={()=>setTemp(true)}>
-                            <Text style={{fontSize:22, color:"black", textAlign:"center"}}>임시 비밀번호 전송</Text>
+                            <Text style={{fontSize:_WIDTH/20, color:"black", textAlign:"center"}}>임시 비밀번호 전송</Text>
                         </PostButton>   
                     }
                     {   
                         tempPW?
                         <PostButton onPress={()=>navigation.navigate('Login')}>
-                            <Text style={{fontSize:22, color:"black", textAlign:"center"}}>로그인하기</Text>
+                            <Text style={{fontSize:_WIDTH/20, color:"black", textAlign:"center"}}>로그인하기</Text>
                         </PostButton>   
                         :null  
                     }
@@ -45,32 +47,32 @@ function FinderPW({navigation}) {
 
 const FinderBox = styled.View`
     width : 90%;
-    height : 300px;
-    padding : 20px 20px 10px 20px;
+    height : ${_HEIGHT*0.4}px;
+    padding : 4% 4% 2% 4%;
     background-color : white;
 `;
 const FinderTitle = styled.TextInput`
     width : 100%;
-    height : 65px;
+    height : ${_HEIGHT/12}px;
     color : black;
-    padding-bottom : 20px;
-    margin-bottom : 40px;
+    padding-bottom : ${_WIDTH/30}px;
+    margin-bottom : ${_WIDTH/15}px;
     text-align : center;
-    font-size : 30px;
+    font-size : ${_WIDTH/16}px;
     font-weight : bold;
-    border-bottom-width : 7px;
+    border-bottom-width : ${_WIDTH/70}px;
 	border-bottom-color : #7f8c8d;
 `;
 const InputData = styled.TextInput`
     width : 95%;
-    height : 55px;
-    padding-left : 15px;
+    height : ${_HEIGHT/15}px;
+    padding-left : ${_WIDTH/25}px;
     border : 2px #7f8c8d ; 
-	font-size : 15px;
+	font-size : ${_WIDTH/32}px;
 `;
 const PostButton = styled.TouchableOpacity`
     width : 88%;
-    height : 60px;
+    height : ${_HEIGHT/12}px;
     margin : 30px 0;
     background-color : white;
     justify-content : center;
